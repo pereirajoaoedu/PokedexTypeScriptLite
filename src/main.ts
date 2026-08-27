27eb6a3a985977d456a459e8ccfde34618644a7e
João Eduardo Pereira <pereira.joaoedu@gmail.com>
2026-08-27 19:43:19 -0300

    feat - Questionar o usuário e buscar Pokemons
    Criada a rotina de questionar ao usuário, a ID ou o nome do Pokemon.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 // Ponto de entrada. Instancia os serviços, injeta as dependências e inicia o loop principal do menu.
+
+import { TerminalController } from './controllers/TerminalController.js';
+import { PokemonApiService } from './services/PokeApiService.js';
+
+async function main(): Promise<void> {
+    const terminalController = new TerminalController();
+    var resposta = await terminalController.iniciar();
+
+    if (resposta) {
+        const pokeApiService = new PokemonApiService();
+        await pokeApiService.buscarPokemon(resposta);
+    }
+
+}
+
+main();
--- src/services/PokeApiService.ts
+++ src/services/PokeApiService.ts
@@ -1,1 +1,23 @@
 // Camada de Integração Externa (fetch nativo). Retorna Promises tipadas com Interfaces.
+
+import type { PokemonApiResponse } from "../interfaces/PokemonApi.js";
+import { PokemonResumido } from "../models/Pokemon.js";
+
+export class PokemonApiService {
+
+    async buscarPokemon(NomeOuId: string): Promise<PokemonResumido[]> {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${NomeOuId.toLowerCase()}`);
+
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar Pokémon com Nome ou ID ${NomeOuId}: ${response.statusText}`);
+            }
+
+            const dados: PokemonApiResponse[] = await response.json();
+            return dados.map((pokemon) => PokemonResumido.fromApiResponse(pokemon));
+        } catch (error) {
+            console.error(`Erro ao buscar Pokémon com Nome ou ID: ${NomeOuId}:`, error);
+            throw error;
+        }
+    }
+}
